a4bf187e8098f35dc09ddace1e0464f4bd193734
Nicole Hollis <nicole@emmabeauty.co>
2019-10-22 14:49:31 -0700

    Added ToDo list component structure

--- 30-before-30/src/Components/ToDoList.js
+++ 30-before-30/src/Components/ToDoList.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import api from "../utils/axiosWithAuth";
+import TodoCard from "./TodoCard";
 
 
-const ToDoList = () => {
-    api.get('/lists').then((res) => console.log(res.data))
+const ToDoList = (props) => {
+
+    api.get('/lists/:id').then((res) => console.log(res.data))
     return (
         <div>
-            
-        </div>
-    )
-}
+        {props.task.map(item => (
+          <TodoCard key={item.id} item={item} toggleItem={props.toggleItem} />
+        ))}
+       
+        <button className="clear-button" onClick={props.clearCompleted}>
+          {console.log(props)}
+          Clear Completed
+        </button>
+       
+      </div>
+    );
+  };
 
 export default ToDoList;
