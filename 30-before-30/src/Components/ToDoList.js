789d2aeae21f9266cfaa9de7d5daea9eeb4eaaec
Raymond Trinh <RaymondTrinh91@gmail.com>
2019-10-23 10:51:51 -0700

    Changes to ToDoList
    Co-Authored-By: Chelsea Wetzel <chelswetz@gmail.com>

--- 30-before-30/src/Components/ToDoList.js
+++ 30-before-30/src/Components/ToDoList.js
@@ -6,19 +6,6 @@ import TodoCard from "./TodoCard";
 const ToDoList = (props) => {
 const [userList, setUserList] = useState([])
 
-  useEffect(()=> {
-    api
-      .get("/lists")
-      .then(response => {
-        console.log(response)
-        setUserList(response.data)
-      })
-      .catch(error => {
-        console.log("This is an error from .GET ToDoList", error)
-      })
-  },[])
-
-
     return (
         <div>
     <TodoCard key={userList.id} item={userList} toggleItem={props.toggleItem} />
